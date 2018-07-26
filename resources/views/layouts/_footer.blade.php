c5448cde7b0ec6e393fd82817f938324b1125094
luffy-gu <873527492@qq.com>
2018-07-26 15:08:06 +0800

    Finish layout and routes

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: W
- * Date: 2018/7/26
- * Time: 14:28
- */
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+            <a href="https://laravel-china.org/courses">
+                刻意练习，每日精进
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-@include('layouts._header')
+@extends('layouts.default')
+@section('title', '注册')
 
-<div class="container">
-    <div class="col-md-offset-1 col-md-10">
-        @yield('content')
-        @include('layouts._footer')
-    </div>
-</div>
-</body>
-</html>
+@section('content')
+    <h1>注册</h1>
+@stop
